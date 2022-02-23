1795835c42f64839bd151255d7a5761d570c1a52
Célestine Feuillat <celestine.feuillat@gmail.com>
2022-02-23 17:18:41 +0100

    Rainbow mode ok

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const content = document.querySelector(".content");
 
-
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
 function make_grid(size) {
     content.innerHTML = '';
@@ -11,7 +18,6 @@ function make_grid(size) {
     }
 
     const lines = document.querySelectorAll(".line")
-
     for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++) {
             let div = document.createElement("div");
@@ -19,18 +25,19 @@ function make_grid(size) {
             lines[i].appendChild(div);
         }
     }
+    draw();
+}
 
+
+function draw() {
     const square = document.querySelectorAll(".square");
-
     square.forEach((el) => {
         el.addEventListener('mouseover', function() {
-            el.style.backgroundColor = "black";
+            el.style.backgroundColor = getRandomColor();
         })
     })
 
 }
-
-make_grid(4);
 
 
 let clear = document.querySelector(".clear");
@@ -39,3 +46,6 @@ clear.addEventListener('click', function() {
     size = Number(prompt("Chose grid size (please enter a number between 2 and 64)"));
     make_grid(size);
 })
+
+
+make_grid(4);
