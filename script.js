bab8332e271df96ece71b3938e9fb3229545fb02
Célestine Feuillat <celestine.feuillat@gmail.com>
2022-02-23 16:58:06 +0100

    V0 working

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
 const content = document.querySelector(".content");
 
-let j = 0;
 
-while (j < 256) {
-    const div = document.createElement("div");
-    div.setAttribute('class', 'line');
-    content.appendChild(div);
-    j += 16;
-}
 
-const line = document.querySelectorAll(".line")
-
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        const div = document.createElement("div");
-        div.setAttribute('class', 'square');
-        line[i].appendChild(div);
+function make_grid(size) {
+    content.innerHTML = '';
+    for (let j = 0; j < size * size; j += size) {
+        let div = document.createElement("div");
+        div.setAttribute('class', 'line');
+        content.appendChild(div);
     }
+
+    const lines = document.querySelectorAll(".line")
+
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            let div = document.createElement("div");
+            div.setAttribute('class', 'square');
+            lines[i].appendChild(div);
+        }
+    }
+
+    const square = document.querySelectorAll(".square");
+
+    square.forEach((el) => {
+        el.addEventListener('mouseover', function() {
+            el.style.backgroundColor = "black";
+        })
+    })
+
 }
 
+make_grid(4);
 
-const square = document.querySelectorAll(".square");
-square.forEach((el) => {
-    el.addEventListener('mouseover', function() {
-        el.style.backgroundColor = "black";
-    })
+
+let clear = document.querySelector(".clear");
+
+clear.addEventListener('click', function() {
+    size = Number(prompt("Chose grid size (please enter a number between 2 and 64)"));
+    make_grid(size);
 })
