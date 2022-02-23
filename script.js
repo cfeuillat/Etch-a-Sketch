6599e61a74d5bad3989228a54f15ee6a9c50515f
Célestine Feuillat <celestine.feuillat@gmail.com>
2022-02-23 15:45:46 +0100

    Grid ok

---
+++ script.js
@@ -0,0 +1,23 @@
+const content = document.querySelector(".content");
+
+let j = 0;
+
+while (j < 256) {
+    const div = document.createElement("div");
+    div.setAttribute('class', 'line');
+    content.appendChild(div);
+    j += 16;
+}
+
+const line = document.querySelectorAll(".line")
+
+for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+        const div = document.createElement("div");
+        div.setAttribute('class', 'square');
+        line[i].appendChild(div);
+    }
+}
+
+
+const square = document.querySelectorAll(".square");
